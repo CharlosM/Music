d4f88d63d53798fa9e209b7e2035de58dca0ab24
CharlosM <50296843+CharlosM@users.noreply.github.com>
2021-06-05 20:22:07 +0200

    Implementation of the frequency domain visualization

--- Python files/20210605_GenScript.py
+++ Python files/20210605_GenScript.py
@@ -1,9 +1,10 @@
 import sounddevice as sd
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.fft as sp
 
 sample_rate = 40*1e3  # in Hertz [Hz]
-recording_time = 5  # in seconds [s]
+recording_time = 10  # in seconds [s]
 
 rec_audio = sd.rec(int(sample_rate * recording_time), sample_rate, 2)
 sd.wait()
@@ -15,3 +16,25 @@ time_vector = time_vector_pre[0:-1]
 
 plt.plot(time_vector, rec_audio)
 plt.show()
+
+# Frequency domain representation of the recorded audio
+#upper_pow2_length = np.ceil(np.log2(total_samples))
+#fft_length = int(2 ** upper_pow2_length)
+
+spectrum_rec_audio_1 = sp.fftshift(sp.fft(rec_audio[:, 0]))
+spectrum_rec_audio_2 = sp.fftshift(sp.fft(rec_audio[:, 1]))
+
+power_spectrum_1 = np.abs(spectrum_rec_audio_1 / total_samples) ** 2
+power_spectrum_2 = np.abs(spectrum_rec_audio_2 / total_samples) ** 2
+
+power_spectrum_1_dBm = 10 * np.log10(power_spectrum_1 / 1e-3)
+power_spectrum_2_dBm = 10 * np.log10(power_spectrum_2 / 1e-2)
+
+freq_vector_pre = np.linspace(-sample_rate/2, sample_rate/2, total_samples+1)
+freq_vector = freq_vector_pre[0:-1]
+
+plt.plot(freq_vector, power_spectrum_1_dBm)
+plt.plot(freq_vector, power_spectrum_2_dBm)
+plt.xlim([0, freq_vector[-1]])
+plt.show()
+
