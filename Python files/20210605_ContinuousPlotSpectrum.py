5155f6d079509a2317ea4d03ba11efe8d8bdc243
CharlosM <50296843+CharlosM@users.noreply.github.com>
2021-06-05 21:02:59 +0200

    Finished implementation for a 'real-time' visualization of the frequency spectrum

---
+++ Python files/20210605_ContinuousPlotSpectrum.py
@@ -0,0 +1,43 @@
+import sounddevice as sd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.fft as sp
+
+sample_rate = 40*1e3  # in Hertz [Hz]
+recording_time = 0.25  # in seconds [s]
+
+run_plot = 1
+ind = 0
+
+while run_plot is 1:
+
+    rec_audio = sd.rec(int(sample_rate * recording_time), sample_rate, 2)
+    sd.wait()
+
+    # Time domain representation of the recorded audio
+    total_samples = len(rec_audio)
+
+    # Frequency domain representation of the recorded audio
+
+    spectrum_rec_audio_1 = sp.fftshift(sp.fft(rec_audio[:, 0]))
+    spectrum_rec_audio_2 = sp.fftshift(sp.fft(rec_audio[:, 1]))
+
+    power_spectrum_1 = np.abs(spectrum_rec_audio_1 / total_samples) ** 2
+    power_spectrum_2 = np.abs(spectrum_rec_audio_2 / total_samples) ** 2
+
+    power_spectrum_1_dB = 10 * np.log10(power_spectrum_1)
+    power_spectrum_2_dB = 10 * np.log10(power_spectrum_2)
+
+    freq_vector_pre = np.linspace(-sample_rate / 2, sample_rate / 2, total_samples + 1)
+    freq_vector = freq_vector_pre[0:-1]
+
+    ind = ind + 1
+    print(ind)
+
+    plt.plot(freq_vector, power_spectrum_1_dB)
+    plt.axis([0, sample_rate / 2, -110, 0])
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('Power [dB]')
+    plt.draw()
+    plt.pause(0.0001)
+    plt.clf()
--- Python files/20210605_GenScript.py
+++ Python files/20210605_GenScript.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import scipy.fft as sp
 
 sample_rate = 40*1e3  # in Hertz [Hz]
-recording_time = 10  # in seconds [s]
+recording_time = 60  # in seconds [s]
 
 rec_audio = sd.rec(int(sample_rate * recording_time), sample_rate, 2)
 sd.wait()
@@ -15,6 +15,9 @@ time_vector_pre = np.linspace(0, recording_time, total_samples+1)
 time_vector = time_vector_pre[0:-1]
 
 plt.plot(time_vector, rec_audio)
+plt.xlim([0, time_vector[-1]])
+plt.xlabel('Time [s]')
+plt.ylabel('Amplitude [a.u.]')
 plt.show()
 
 # Frequency domain representation of the recorded audio
@@ -27,14 +30,16 @@ spectrum_rec_audio_2 = sp.fftshift(sp.fft(rec_audio[:, 1]))
 power_spectrum_1 = np.abs(spectrum_rec_audio_1 / total_samples) ** 2
 power_spectrum_2 = np.abs(spectrum_rec_audio_2 / total_samples) ** 2
 
-power_spectrum_1_dBm = 10 * np.log10(power_spectrum_1 / 1e-3)
-power_spectrum_2_dBm = 10 * np.log10(power_spectrum_2 / 1e-2)
+power_spectrum_1_dB = 10 * np.log10(power_spectrum_1)
+power_spectrum_2_dB = 10 * np.log10(power_spectrum_2)
 
 freq_vector_pre = np.linspace(-sample_rate/2, sample_rate/2, total_samples+1)
 freq_vector = freq_vector_pre[0:-1]
 
-plt.plot(freq_vector, power_spectrum_1_dBm)
-plt.plot(freq_vector, power_spectrum_2_dBm)
+plt.plot(freq_vector, power_spectrum_1_dB)
+plt.plot(freq_vector, power_spectrum_2_dB)
 plt.xlim([0, freq_vector[-1]])
+plt.xlabel('Frequency [Hz]')
+plt.ylabel('Power [dB]')
 plt.show()
 
